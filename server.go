ac732d4f2b5bf12bc4f4a09f61b90aded5556187
Satvik <satvik.manchanda@univest.in>
2025-08-31 21:31:49 +0530

    feat: add file deletion functionality across all servers

--- server.go
+++ server.go
@@ -79,9 +79,14 @@ type MessageGetFile struct {
 	Key string
 }
 
+type MessageDeleteFile struct {
+	Key string
+}
+
 func init() {
 	gob.Register(MessageStoreFile{})
 	gob.Register(MessageGetFile{})
+	gob.Register(MessageDeleteFile{})
 }
 
 func (s *FileServer) broadcast(msg *Message) error {
@@ -133,6 +138,25 @@ func (s *FileServer) Get(key string) ( io.Reader, error) {
 	}
 	 _, r, err := s.store.Read(key)
 	 return r, err
+}
+
+func (s *FileServer) DeleteFromEveryServer(key string) error{
+	if(!s.store.Has(key)){
+		return fmt.Errorf("do not have the file ");
+	}
+	s.store.Delete(key);
+	msg := Message{
+		Payload: MessageDeleteFile{
+			Key: key,
+		},
+	}
+
+	if err := s.broadcast(&msg); err != nil {
+		log.Printf("Failed to broadcast message: %v", err)
+		return err
+	}
+
+	return nil
 }
 
 func (s *FileServer) Store(key string, r io.Reader) error {
@@ -265,6 +289,8 @@ func (s *FileServer) handleMessage(from string, msg *Message) error {
 		s.handleMessageStorageFile(from, payload)
 	case MessageGetFile:
 		s.handleMessageGetFile(from, payload)
+	case MessageDeleteFile:
+		s.handleMessageDeleteFile(from, payload)
 	default:
 		log.Printf("Unknown message type: %T", payload)
 		return nil
@@ -291,6 +317,22 @@ func (s *FileServer) handleMessageStorageFile(from string, msg MessageStoreFile)
 	 
 	return nil
 
+}
+
+func (s *FileServer) handleMessageDeleteFile(from string, msg MessageDeleteFile) error {
+	if !s.store.Has(msg.Key) {
+		return fmt.Errorf("%s need to delete file %s from disk and it doesn't exist", s.Ops.Transport.Addr(), msg.Key)
+	}
+
+	fmt.Printf("%s deleting file %s over the network\n", s.Ops.Transport.Addr(), msg.Key)
+
+	if err := s.store.Delete(msg.Key); err != nil {
+		fmt.Printf("Failed to delete file %s from store: %v\n", msg.Key, err)
+		return err
+	}
+
+	fmt.Printf("%s Deleted file %s from store\n", s.Ops.Transport.Addr(), msg.Key)
+	return nil
 }
 
 func (s *FileServer) Stop() {
